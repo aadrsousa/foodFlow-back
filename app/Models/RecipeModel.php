7b1bcf75088d2183cd78904887796d76d69dd301
Alex Sousa <alexalarcon@outlook.com>
2024-04-15 19:54:15 +0200

    Added the recipe controller and model

---
+++ app/Models/RecipeModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class RecipeModel extends Model
+{
+    protected $table = 'recipes';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['title', 'description', 'ingredients', 'parent_recipe_id'];
+    protected $returnType = 'array';
+    protected $useTimestamps = false;
+}
