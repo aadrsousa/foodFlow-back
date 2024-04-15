b7b90547d1be65f2feee4cf793b77b3e64c88611
Alex Sousa <alexalarcon@outlook.com>
2024-04-15 19:54:36 +0200

    Implemented the controllers with Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,5 +18,26 @@ $routes->put('centres/(:num)', 'CentresController::update/$1');
 $routes->delete('centres/(:num)', 'CentresController::delete/$1');
 
 // USERS
-$routes->post('register', 'UserController::register');
-$routes->post('login', 'UserController::login');
+$routes->post('users/register', 'UserController::register');
+$routes->post('users/login', 'UserController::login');
+
+// PROVIDERS
+$routes->get('providers', 'ProviderController::index');
+$routes->get('providers/(:num)', 'ProviderController::show/$1');
+$routes->post('providers', 'ProviderController::create');
+$routes->put('providers/(:num)', 'ProviderController::update/$1');
+$routes->delete('providers/(:num)', 'ProviderController::delete/$1');
+
+// RECIPES
+$routes->get('recipes', 'RecipeController::index');
+$routes->get('recipes/(:num)', 'RecipeController::show/$1');
+$routes->post('recipes', 'RecipeController::create');
+$routes->put('recipes/(:num)', 'RecipeController::update/$1');
+$routes->delete('recipes/(:num)', 'RecipeController::delete/$1');
+
+// MENUS
+$routes->get('menus', 'MenuController::index');
+$routes->get('menus/(:num)', 'MenuController::show/$1');
+$routes->post('menus', 'MenuController::create');
+$routes->put('menus/(:num)', 'MenuController::update/$1');
+$routes->delete('menus/(:num)', 'MenuController::delete/$1');
